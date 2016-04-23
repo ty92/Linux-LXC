e4241f6e5fb1b468fa6e36b3544f76f5ac82a1b3
ty <tianye04@qq.com>
2016-04-23 13:48:59 +0800

    namespace 学习代码

---
+++ namespace/namespace-ipc.c
@@ -0,0 +1,55 @@
+/**
+ * 此程序执行需要使用root权限，因为涉及到uts
+ * CLONE_NEWIPC隔离信号量、共享内存、消息队列，不隔离管道pipe；
+ * father process create pipe,father process sleep(4)，
+ * child process 先执行，关闭管道写端，read 管道checkpoint[0],阻塞
+ * 父进程sleep over，关闭写端(触发一个“close”事件)，传输“EOF”结束符到child的读取端
+ * child接收到"EOF"后，继续运行后边的语句。
+ *
+ * 该程序主要是隔离IPC（进程间通信），但是不隔离pipe通信，此处程序使用管道主要是为了体现出
+ * 隔离了IPC，但是还可以使用管道通信
+ **/
+#define _GNU_SOURCE
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <sched.h>
+#include <signal.h>
+#include <unistd.h>
+
+#define STACK_SIZE (1024 * 1024)
+
+int checkpoint[2];
+static char child_stack[STACK_SIZE];
+char * const child_args[] = {
+	"/bin/bash",
+	NULL
+};
+
+int child_main(void* arg) {
+	char c;
+	
+	close(checkpoint[1]);
+	read(checkpoint[0], &c, 1);
+	printf("- world !\n");
+	sethostname("In Namespace",12);
+	execv(child_args[0], child_args);
+	printf("EXECV ERROR\n");
+	exit(EXIT_FAILURE);
+}
+
+int main()
+{
+	pipe(checkpoint);
+
+	printf("- Hello ? \n");
+	int child_pid = clone(child_main, child_stack + STACK_SIZE, CLONE_NEWUTS | CLONE_NEWIPC | SIGCHLD, NULL);
+	if(child_pid == -1)
+		perror("clone");
+	
+	sleep(4);		//parent睡眠，子进程读管道（zuse），等到父进程唤醒，关闭写端，子进程从管道接收到关闭管道消息，继续执行后边语句
+	close(checkpoint[1]);	
+	waitpid(child_pid, NULL, 0);
+	printf("father\n");
+	exit(EXIT_SUCCESS);
+}
---
+++ namespace/namespace-ns.c
@@ -0,0 +1,52 @@
+/**
+ * 使用CLONE_NEWNS隔离挂载点，涉及到挂载私有属性的问题
+ * Centos挂载有私有属性问题 shared slave private三个属性；
+ * 只有将其设置为private才会真正实现挂载隔离：mount --make-private /proc,设置/proc目录挂载属性为private；
+ * 当挂载属性不为private时，子进程挂载后未umount直接exit时，会影响其他进程使用，
+ * 此时可以在其它进程时重新挂载一次即可mount -t proc proc /proc
+ **/
+#define _GNU_SOURCE
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <sched.h>
+#include <signal.h>
+#include <unistd.h>
+
+#define STACK_SIZE (1024 * 1024)
+
+int checkpoint[2];
+static char child_stack[STACK_SIZE];
+char * const child_args[] = {
+	"/bin/bash",
+	NULL
+};
+
+int child_main(void* arg) {
+	char c;
+	
+	close(checkpoint[1]);
+	printf("- [%5d] world \n",getpid());
+	sethostname("In Namespace",12);
+//	mount("proc", "/proc", "proc", 0, NULL);
+	read(checkpoint[0], &c, 1);
+	execv(child_args[0], child_args);
+	printf("EXECV ERROR\n");
+	exit(EXIT_FAILURE);
+}
+
+int main()
+{
+	pipe(checkpoint);
+
+	printf("- [%5d] Hello ? \n",getpid());
+	int child_pid = clone(child_main, child_stack + STACK_SIZE, CLONE_NEWUTS | CLONE_NEWIPC | CLONE_NEWPID | CLONE_NEWNS | SIGCHLD, NULL);
+	if(child_pid == -1)
+		perror("clone");
+	
+	sleep(4);
+	close(checkpoint[1]);
+	waitpid(child_pid, NULL, 0);
+	printf("father\n");
+	exit(EXIT_SUCCESS);
+}
